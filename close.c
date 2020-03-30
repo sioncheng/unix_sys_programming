2180a69cf3b107eceb9e5fcb5d5453d21b38300e
sion <sion@SionMacBookPro.local>
2020-03-30 11:03:11 +0800

    errno

--- close.c
+++ close.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<string.h>
+#include<errno.h>
 
 int main() {
 	int fildes;
 
-	if (close(fildes) != 0) 
-		perror(NULL);
+	int r;
+
+	r = close(fildes);
+
+	fprintf(stderr, "%d\n", errno);
+
+	if (r != 0) 
+		fprintf(stderr, "Failed to close file %d: %s\n", fildes, strerror(errno));
+
+	fprintf(stderr, "%d\n", errno);
 
 	return 0;
 }
